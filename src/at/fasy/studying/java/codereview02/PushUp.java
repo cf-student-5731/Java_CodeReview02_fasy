e03ae4b52c3f2db349a8ad7bbb989c9de3ec019c
fyzam <fyzam@fasy.at>
2020-07-11 16:13:06 +0200

    unnecessary test class removed

--- src/at/fasy/studying/java/codereview02/PushUp.java
+++
@@ -1,8 +0,0 @@
-package at.fasy.studying.java.codereview02;
-
-public class PushUp extends Exercise{
-
-    PushUp(String name, String description, int duration, Positions position) {
-        super(name, description, duration, position);
-    }
-}
